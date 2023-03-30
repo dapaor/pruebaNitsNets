6c51bf163a28ee092a74e694ee7588befa4f4728
David <dpo4@alu.ua.es>
2023-03-30 22:00:36 +0200

    feature(migrations): added migrations for Deportes, Pistas, Reservas and Socios. Edited Users migration

---
+++ database/migrations/2023_03_30_194647_pistas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('pistas', function(Blueprint $table){
+            $table->id('pista_id');
+            $table->text('codigo')->unique();
+            $table->float('ancho');
+            $table->float('largo');
+            $table->foreignId('deporte_id');
+
+            $table->index('codigo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2023_03_30_194652_reservas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('reservas', function(Blueprint $table){
+            $table->id('reserva_id');
+            $table->foreignId('deporte_id');
+            $table->foreignId('pista_id');
+            $table->dateTime('dia');
+            $table->integer('hora');
+
+            $table->index('dia');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
