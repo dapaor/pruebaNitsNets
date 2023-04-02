f69f17ea2ccfec5aae712d3068e25b0c0c873f40
David <dpo4@alu.ua.es>
2023-04-02 19:18:56 +0200

    feature(ReservaModel): added cast to date for dia

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,6 +13,7 @@ class Reserva extends Model
     protected $table = 'reservas';
 
     protected $casts = [
-      'dia' => 'datetime'
+      'dia' => 'date:d-m-Y'
     ];
+
 }
